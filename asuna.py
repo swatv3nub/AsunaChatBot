4a0c4ada7884d8bfb6a388609e13ff55265ed62b
sᴡᴏɴɪᴛ <swatv3nub@gmail.com>
2021-04-23 00:25:19 +0530

    Update asuna.py

--- asuna.py
+++ asuna.py
@@ -36,7 +36,7 @@ start_text = """Hello, I am **Asuna [アスナ]**, An Intelligent ChatBot. If Yo
 info_text = f"""Build To Be One of The Cutest ChatBot,\n**Asuna [アスナ]** is One of the Anime Themed Chatbot in Telegram.
 
 **Asuna [アスナ]** System Info:
-    **• Host:** `Heroku` [Can Die Anytime]
+    **• Host:** `Heroku`
     **• OS:** `Debian`
     **• Python Version:** `3.9.1`
     **• Library Used:** `Pyrogram`
@@ -44,6 +44,8 @@ info_text = f"""Build To Be One of The Cutest ChatBot,\n**Asuna [アスナ]** is
     **• Owner | Bot Dev:** @MaskedVirus
     **• Chatbot API Dev:** @Madepranav
     """
+    
+asunapic = "https://telegra.ph/file/cb3b9a091ee2480fc2cea.jpg"
     
 keyboard = InlineKeyboardMarkup(
             [
@@ -70,19 +72,19 @@ helpo = InlineKeyboardMarkup(
             ]
         )        
 
-@asuna.on_mesaage(filters.command("start"))
+@asuna.on_message(filters.command("start"))
 async def start(_, message):
-    asunapic = "https://telegra.ph/file/cb3b9a091ee2480fc2cea.jpg"
     if message.chat.type == "private":
-        message.reply_photo(photo=asunapic, caption=start_text, reply_markup=keyboard, parse_mode="markdown")
+        await message.reply_photo(photo=asunapic, caption=start_text, reply_markup=keyboard, parse_mode="markdown")
     else:
-        message.reply_text("**Asuna [アスナ]** is Alive\nowo :3", parse_mode="markdown")
+        await message.reply_text("**Asuna [アスナ]** is Alive\nowo :3", parse_mode="markdown")
 
 @asuna.on_callback_query(filters.regex("start_back"))
 async def start_back(_, CallbackQuery):
-    await asuna.send_message(
+    await asuna.send_photo(
         CallbackQuery.message.chat.id,
-        text=start_text,
+        photo=asunapic,
+        caption=start_text,
         reply_markup=keyboard
     )
 
@@ -91,9 +93,10 @@ async def start_back(_, CallbackQuery):
 
 @asuna.on_callback_query(filters.regex("info"))
 async def start_info(_, CallbackQuery):
-    await app.send_message(
+    await asuna.send_photo(
         CallbackQuery.message.chat.id,
-        text=info_text,
+        photo=asunapic,
+        caption=info_text,
         reply_markup=helpo
     )
 
@@ -104,26 +107,8 @@ async def start_info(_, CallbackQuery):
 async def chat(_, message):
     if message.chat.type == "private":
         if not message.text:
-        return
-    query = message.text
-    if len(query) > 50:
-        return
-    try:
-        res = await chatbot(query)
-        await asyncio.sleep(1)
-    except Exception as e:
-        res = str(e)
-    await message.reply_text(res)
-    await asuna.send_chat_action(message.chat.id, "cancel")
-    if message.chat.type != "private":
-        if message.reply_to_message:
-            if not message.reply_to_message.from_user.id == bot_id:
             return
-        await asuna.send_chat_action(message.chat.id, "typing")
-        if not message.text:
-            query = "Hello"
-        else:
-            query = message.text
+        query = message.text
         if len(query) > 50:
             return
         try:
@@ -133,13 +118,17 @@ async def chat(_, message):
             res = str(e)
         await message.reply_text(res)
         await asuna.send_chat_action(message.chat.id, "cancel")
-    else:
-        if message.text:
-            query = message.text
-            if len(query) > 50:
-                return
-            if re.search("[.|\n]{0,}[a|A][s|S][u|U][n|N][a|A][.|\n]{0,}", query):
+        if message.chat.type != "private":
+            if message.reply_to_message:
+                if not message.reply_to_message.from_user.id == bot_id:
+                    return
                 await asuna.send_chat_action(message.chat.id, "typing")
+                if not message.text:
+                    query = "Hello"
+                else:
+                    query = message.text
+                if len(query) > 50:
+                    return
                 try:
                     res = await chatbot(query)
                     await asyncio.sleep(1)
@@ -147,7 +136,20 @@ async def chat(_, message):
                     res = str(e)
                 await message.reply_text(res)
                 await asuna.send_chat_action(message.chat.id, "cancel")
-
+            else:
+                if message.text:
+                    query = message.text
+                    if len(query) > 50:
+                        return
+                    if re.search("[.|\n]{0,}[a|A][s|S][u|U][n|N][a|A][.|\n]{0,}", query):
+                        await asuna.send_chat_action(message.chat.id, "typing")
+                        try:
+                            res = await chatbot(query)
+                            await asyncio.sleep(1)
+                        except Exception as e:
+                            res = str(e)
+                        await message.reply_text(res)
+                        await asuna.send_chat_action(message.chat.id, "cancel")
 
 print(
     """
